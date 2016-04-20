bcfdf6eb1ef9bd9a08faa1f20d3ed2b2f72405e0
kbl212 <kyle.lauritzen@gmail.com>
2016-04-19 21:15:01 -0600

    file creation and file writing with PHP

--- index.php
+++ index.php
@@ -1,16 +1,47 @@
+<html>
+    <head>
+        <title>
+            Cool Title
+        </title>
+    </head>
+    <body>
+        
+    </body>
+</html>
+
+
 <?php
+
+    class Example 
+    {
+        public $age = "Michael";
+    }
+
     $username = "Kyle Lauritzen";
     echo $username;
     echo "<br><br>";
     $current_user = $username;
     echo $current_user;
     echo "<br><br>";
-    $team = array('Kyle', 'Fletcher', 'Dylan', 'David');
+    $team = array('Kyle', 'Fletcherini', 'Dylan', 'David', 'Suzaon');
     $name_paragraph = '<p style="background-color:red;">' . $current_user . '</p>';
     echo $name_paragraph;
     for ($i = 0; $i < count($team); $i++) 
     {
         echo $team[$i];
+        
         echo "<br><br>";
     }
+    $lowercaseString = strtolower("HAHAHA");
+    echo $lowercaseString;
+    echo ucfirst("<br><br>" . "testing");
+
+
+    
+    $fh = fopen("testfile.txt", "w") or die("Failed to create file");
+    $text = "testing. HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA";
+
+    fwrite($fh, $text) or die("Could not write to file");
+    fclose($fh);
+    echo "File testfile.txt written successfully";
 ?>
